82c45e8bd9d05ef9fa74712ae848f45d976bb7bb
3011 <choybb@qq.com>
2022-03-13 10:15:15 +0800

    update

--- leetcode/cpp/231.2-的幂.cpp
+++ leetcode/cpp/231.2-的幂.cpp
@@ -11,7 +11,10 @@ public:
     bool isPowerOfTwo(int n)
     {
         // n 与 -n （补码）按位与后应该相同等于它本身。
-        return n > 0 && (n & -n) == n;
+        // return n > 0 && (n & -n) == n;
+
+        // n 二进制最高位为 1，其余所有位为 0；n−1 二进制最高位为 0，其余所有位为 1；
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
 // @lc code=end
