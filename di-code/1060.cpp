391d3a8f9ce4fc882d1c66facfe90b3214ab6b82
3011 <choybb@qq.com>
2022-03-18 11:06:58 +0800

    update

---
+++ di-code/1060.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    for (int i = 100; i < 1000; i++)
+    {
+        num = pow(i / 100 % 10, 3) + pow(i / 10 % 10, 3) + pow(i % 10, 3);
+        if (i == num)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
