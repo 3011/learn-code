67d4bdb725765298531e20edf411204c258f711d
3011 <choybb@qq.com>
2022-03-18 13:43:19 +0800

    update

---
+++ di-code/other/4-6-2-打印乘法九九表.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    for (int i = 0; i <= 9; i++)
+    {
+        if (i == 0)
+        {
+            cout << left << setw(5) << '*';
+            continue;
+        }
+
+        cout << left << setw(5) << i;
+    }
+    cout << endl;
+
+    cout << setfill('.') << setw(49) << "." << setfill(' ') << endl;
+
+    for (x = 1; x <= 9; x++)
+    {
+        cout << left << setw(5) << x;
+        for (y = 1; y <= x; y++)
+        {
+            cout << left << setw(5) << x * y;
+        }
+        cout << endl
+             << endl;
+    }
+
+    return 0;
+}
