f01e31272536629bef75c82037b9869dec06434a
3011 <choybb@qq.com>
2022-03-25 11:48:11 +0800

    commit

---
+++ di-code/3534.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isprime(int m)
+{
+    if (m <= 3)
+    {
+        return m > 1;
+    }
+
+    for (int i = 2; i < m; i++)
+    {
+        if (m % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << (isprime(n) ? "True" : "False") << endl;
+
+    return 0;
+}
