327127793301ae0378c350c35f58109cf6819e8a
3011 <choybb@qq.com>
2022-04-01 11:58:19 +0800

    update

---
+++ di-code/other/6-3-学生课程分数.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void find_best()
+{
+}
+
+void find_failed()
+{
+}
+
+void get_average_score() {}
+
+int main()
+{
+    int score[5][4] = {{100, 98, 100, 90},
+                       {90, 95, 93, 100},
+                       {80, 75, 90, 100},
+                       {50, 70, 80, 55},
+                       {58, 55, 60, 60}};
+
+    
+}
