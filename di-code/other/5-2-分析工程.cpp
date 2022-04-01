593e94a3afa63b2bb0a505d2be528ea47905805b
3011 <choybb@qq.com>
2022-04-01 11:05:37 +0800

    update

--- di-code/other/5-2-分析工程.cpp
+++ di-code/other/5-2-分析工程.cpp
@@ -6,22 +6,22 @@ int n = 1;
 //---------------------
 int main()
 {
-    static int x = 5;
-    int y = n; //y = 1
-    cout << "Main--x=" << x
-         << ", y=" << y << "，n=" << n << endl;
-    func();
-    cout << "Main--x=" << x
-         << ", y=" << y << "，n=" << n << endl;
-    func();
+     static int x = 5;
+     int y = n;
+     cout << "Main--x=" << x
+          << ", y=" << y << "，n=" << n << endl;
+     func();
+     cout << "Main--x=" << x
+          << ", y=" << y << "，n=" << n << endl;
+     func();
 } //--------------------
 void func()
 {
-    static int x = 4;
-    int y = 10;
-    x += 2;  // x = 6
-    n += 10; // n = 11
-    y += n; // y = 21 
-    cout << "Func--x=" << x
-         << ", y=" << y << "，n=" << n << endl;
+     static int x = 4;
+     int y = 10;
+     x += 2;
+     n += 10;
+     y += n;
+     cout << "Func--x=" << x
+          << ", y=" << y << "，n=" << n << endl;
 } //--------------------
