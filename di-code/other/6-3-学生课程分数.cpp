b5ae48bc1213eed82aeb8eb6471a4f17992db665
3011 <choybb@qq.com>
2022-04-01 13:54:48 +0800

    update

--- di-code/other/6-3-学生课程分数.cpp
+++ di-code/other/6-3-学生课程分数.cpp
@@ -1,23 +1,89 @@
 #include <iostream>
 using namespace std;
 
-void find_best()
+void find_best(int score[], int num_students, int num_courses)
 {
+    int m, n;
+    int best_student;
+    cout << "(Course) Best student:" << endl;
+    for (m = 0; m < num_courses; m++)
+    {
+        best_student = 0;
+        for (n = 0; n < num_students; n++)
+        {
+            if (score[n * num_courses + m] > score[best_student * num_courses + m])
+                best_student = n;
+        }
+        cout << m + 1 << ": ";
+        for (n = 0; n < num_students; n++) // 可能有同学都是最高分
+        {
+            if (score[n * num_courses + m] == score[best_student * num_courses + m])
+                cout << "Student " << n + 1 << " (" << score[n * num_courses + m] << ") ";
+        }
+        cout << endl;
+    }
 }
 
-void find_failed()
+void find_failed(int score[], int num_students, int num_courses)
 {
+    int m, n;
+    bool has_failed;
+
+    cout << "(Student) Failed:" << endl;
+    for (m = 0; m < num_students; m++)
+    {
+        has_failed = false;
+        for (n = 0; n < num_courses; n++)
+        {
+            if (score[m * num_courses + n] < 60)
+            {
+                has_failed = true;
+                break;
+            }
+        }
+        if (has_failed)
+        {
+            cout << m + 1 << ": ";
+            for (n = 0; n < num_courses; n++)
+            {
+                cout << score[m * num_courses + n] << " ";
+            }
+            cout << endl;
+        }
+    }
 }
 
-void get_average_score() {}
+void get_average_score(int score[], int num_students, int num_courses)
+{
+    int m, n;
+    float total_score;
+    cout << "(Student) Average score:" << endl;
+    for (m = 0; m < num_students; m++)
+    {
+        total_score = 0;
+        for (n = 0; n < num_courses; n++)
+        {
+            total_score += score[m * num_courses + n];
+        }
+        cout << m + 1 << ": " << total_score / num_courses << endl;
+    }
+}
 
 int main()
 {
-    int score[5][4] = {{100, 98, 100, 90},
-                       {90, 95, 93, 100},
-                       {80, 75, 90, 100},
-                       {50, 70, 80, 55},
-                       {58, 55, 60, 60}};
+    const int num_students = 5;
+    const int num_courses = 4;
+    int score[num_students * num_courses] = {100, 98, 100, 90,
+                                             90, 95, 93, 100,
+                                             80, 75, 90, 100,
+                                             50, 70, 80, 55,
+                                             58, 55, 60, 60};
 
-    
+    find_best(score, num_students, num_courses);
+    cout << endl;
+    find_failed(score, num_students, num_courses);
+    cout << endl;
+    get_average_score(score, num_students, num_courses);
+
+    return 0;
 }
---
+++ di-code/other/6-4-矩阵相加.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int matrix_size = 3 * 3;
+    int matrix_1[matrix_size] = {5, 7, 8, 2, -2, 4, 1, 1, 1};
+    int matrix_2[matrix_size] = {4, -2, 3, 3, 9, 4, 8, -1, 2};
+    int matrix[matrix_size];
+    for (int i = 0; i < matrix_size; i++)
+    {
+        matrix[i] = matrix_1[i] + matrix_2[i];
+    }
+    for (int i = 0; i < matrix_size; i++)
+    {
+        if (i % 3 == 0)
+            cout << endl;
+        cout << matrix[i] << " ";
+    }
+
+    return 0;
+}
