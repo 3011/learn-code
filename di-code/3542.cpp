4ff82d58f9b34d616c1ba0376b29626b90b9e48c
3011 <choybb@qq.com>
2022-04-01 11:44:00 +0800

    update

--- di-code/3542.cpp
+++ di-code/3542.cpp
@@ -8,13 +8,13 @@ int main()
     int *matrix = new int[n * n];
 
     int i;
-    for (i = 0; i < n * n; i++)
+    for (i = 0; i < n * n; i++) // 输入矩阵内容
     {
         cin >> matrix[i];
     }
-    
+
     int count = 0;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++) // 求对角线之和
     {
         count += matrix[i * (n + 1)] + matrix[(i + 1) * (n - 1)];
     }
