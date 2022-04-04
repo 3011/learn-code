259112ebaeda22e44445aed92a0e269c386ec35c
3011 <choybb@qq.com>
2022-04-04 12:37:42 +0800

    update

--- di-code/other/6-4-矩阵相加.cpp
+++ di-code/other/6-4-矩阵相加.cpp
@@ -4,13 +4,20 @@ using namespace std;
 int main()
 {
     const int matrix_size = 3 * 3;
-    int matrix_1[matrix_size] = {5, 7, 8, 2, -2, 4, 1, 1, 1};
-    int matrix_2[matrix_size] = {4, -2, 3, 3, 9, 4, 8, -1, 2};
+    int matrix_1[matrix_size] = {5, 7, 8,
+                                 2, -2, 4,
+                                 1, 1, 1};
+    int matrix_2[matrix_size] = {4, -2, 3,
+                                 3, 9, 4,
+                                 8, -1, 2};
     int matrix[matrix_size];
+
+    // 矩阵相加
     for (int i = 0; i < matrix_size; i++)
     {
         matrix[i] = matrix_1[i] + matrix_2[i];
     }
+
     for (int i = 0; i < matrix_size; i++)
     {
         if (i % 3 == 0)
