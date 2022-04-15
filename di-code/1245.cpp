8a500d32d37aea42882ee248808b44334fe9cee0
3011 <choybb@qq.com>
2022-04-15 09:13:20 +0800

    upate

---
+++ di-code/1245.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    double sum, k;
+    for (k = 1; k <= a; k++)
+    {
+        sum += k;
+    }
+    for (k = 1; k <= b; k++)
+    {
+        sum += k * k;
+    }
+    for (k = 1; k <= c; k++)
+    {
+        sum += 1 / k;
+    }
+    cout << fixed << setprecision(2) << sum;
+
+    return 0;
+}
