a616c5398d5fe2aca44f30a1ffda6e23c1ae32dd
3011 <choybb@qq.com>
2022-04-15 09:47:20 +0800

    update

---
+++ di-code/1275.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void input(int *nums)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> nums[i];
+    }
+}
+
+void handle(int *nums)
+{
+    int most_index = 0;
+    for (int i = 1; i < 10; i++)
+        most_index = nums[most_index] < nums[i] ? most_index : i;
+    if (most_index != 0)
+    {
+        nums[most_index] ^= nums[0];
+        nums[0] ^= nums[most_index];
+        nums[most_index] ^= nums[0];
+    }
+
+    most_index = 0;
+    for (int i = 1; i < 10; i++)
+        most_index = nums[most_index] > nums[i] ? most_index : i;
+    if (most_index != 0)
+    {
+        nums[most_index] ^= nums[9];
+        nums[9] ^= nums[most_index];
+        nums[most_index] ^= nums[9];
+    }
+}
+
+void output(int *nums)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int nums[10];
+    input(nums);
+    handle(nums);
+    output(nums);
+    
+    return 0;
+}
---
+++ lanqiao/2022-B/105-X进制减法.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, ma_num, mb_num, i;
+    cin >> n >> ma_num;
+
+    int *ma = new int[ma_num];
+    for (i = 0; i < ma_num; i++)
+    {
+        cin >> ma[i];
+    }
+
+    cin >> mb_num;
+
+    int *mb = new int[mb_num];
+    for (i = 0; i < mb_num; i++)
+    {
+        cin >> mb[i];
+    }
+
+    for (i = 0; i < ma_num; i++)
+    {
+        
+    }
+    
+
+
+    return 0;
+}
