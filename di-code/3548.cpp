31a15f8d41ff80a6ab98dbdb7badbae6d477138a
3011 <choybb@qq.com>
2022-05-03 11:58:42 +0800

    update

--- di-code/3548.cpp
+++ di-code/3548.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void swap(char *x, char *y)
+void swap_ch(char *x, char *y)
 {
     *x ^= *y;
     *y ^= *x;
@@ -16,7 +16,7 @@ int main()
     cout << "before:" << ch1 << " " << ch2 << endl;
 
     //此处调用swap_ch函数交换ch1和ch2
-    swap(ch1, ch2);
+    swap_ch(&ch1, &ch2);
 
     cout << "after:" << ch1 << " " << ch2 << endl;
 
