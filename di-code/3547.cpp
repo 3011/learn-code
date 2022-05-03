7174311af6075cfd087446c000fce41b751a7af7
3011 <choybb@qq.com>
2022-05-03 11:55:00 +0800

    update

--- di-code/3547.cpp
+++ di-code/3547.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-void stringcmp(string str1, string str2)
+void stringcmp(char *str1, char *str2)
 {
-    if (str1 > str2)
+    int i = 0;
+    while (str1[i] == str2[i])
+    {
+        if (str1[i] == '\0' || str2[i] == '\0')
+            break;
+        i++;
+    }
+
+    if (int(str1[i]) > int(str2[i]))
         cout << str1 << ">" << str2 << endl;
-    else if (str1 < str2)
+    else if (str1[i] < str2[i])
         cout << str1 << "<" << str2 << endl;
-    else if (str1 == str2)
+    else if (str1[i] == str2[i])
         cout << str1 << "==" << str2 << endl;
+    else if (str1[i] == '\0')
+        cout << str1 << "<" << str2 << endl;
+    else if (str2[i] == '\0')
+        cout << str1 << ">" << str2 << endl;
 }
 
 int main()
 {
-    string str1, str2;
+    char str1[100], str2[100];
     cin >> str1 >> str2;
     stringcmp(str1, str2);
 
