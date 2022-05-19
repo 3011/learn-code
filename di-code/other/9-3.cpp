d9b204483485af7fea2738e3531e7fe86b0f578d
3011 <choybb@qq.com>
2022-05-20 00:02:07 +0800

    update

--- di-code/other/9-3.cpp
+++ di-code/other/9-3.cpp
@@ -7,35 +7,59 @@ struct node
     string data;
     node *next;
 
-    node(string s) : data(s), next(NULL) {}
     node() : data(""), next(NULL) {}
+    node(string s) : data(s), next(NULL) {}
 };
 
 void addNode(node *head, string data)
 {
     node *newNode = new node(data);
 
-    node *p = new node;
-    p = head;
+    node *p = head;
     while (p->next != NULL)
         p = p->next;
 
     p->next = newNode;
 }
 
+void insertMarit(node *head)
+{
+    node *newNode = new node("marit");
+
+    node *p = head;
+    while (p->next != NULL)
+    {
+        if (p->next->data == "jone")
+        {
+            newNode->next = p->next;
+            p->next = newNode;
+            return;
+        }
+        p = p->next;
+    }
+
+    p->next = newNode;
+}
+
+void printNode(node *head)
+{
+    cout << "链表中的元素依次为：" << endl;
+    while (head->next != NULL, head = head->next)
+        cout << head->data << " ";
+}
+
 int main()
 {
-    node *head = new node("marit");
+    node *head = new node();
 
     string cin_data;
     cout << "请输入姓名（以0结束）：" << endl;
     while (cin >> cin_data, cin_data != "0")
         addNode(head, cin_data);
 
-    cout << "链表中的元素依次为：" << endl;
-    do
-        cout << head->data << " ";
-    while (head->next != NULL, head = head->next);
+    insertMarit(head);
+
+    printNode(head);
 
     return 0;
 }
